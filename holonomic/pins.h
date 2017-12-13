ec9e21b8176b9680642a588d15d65c27992fecbd
Tim Doerzbacher <tim@tim-doerzbacher.com>
2017-12-12 19:31:58 -0500

    Add placeholder for storing pin out information

---
+++ holonomic/pins.h
@@ -0,0 +1,43 @@
+#ifndef __CATPOO_PINS_H__
+#define __CATPOO_PINS_H__
+
+/****************************************************************************
+ * Motor pin out constants.
+ *
+ * Double check and adjust these as necessary for your project.
+ *
+ ***************************************************************************/
+
+//
+// Left Motor
+//
+#define MOTOR_1_PIN_1 A0
+#define MOTOR_1_PIN_2 A1
+#define MOTOR_1_PIN_3 A2
+#define MOTOR_1_PIN_4 A3
+
+//
+// Front Motor
+//
+#define MOTOR_2_PIN_1 A4
+#define MOTOR_2_PIN_2 A5
+#define MOTOR_2_PIN_3 4
+#define MOTOR_2_PIN_4 5
+
+//
+// Right Motor
+//
+#define MOTOR_3_PIN_1 6
+#define MOTOR_3_PIN_2 7
+#define MOTOR_3_PIN_3 8
+#define MOTOR_3_PIN_4 9
+
+//
+// Back Motor
+//
+#define MOTOR_4_PIN_1 10
+#define MOTOR_4_PIN_2 11
+#define MOTOR_4_PIN_3 12
+#define MOTOR_4_PIN_4 13
+
+#endif
