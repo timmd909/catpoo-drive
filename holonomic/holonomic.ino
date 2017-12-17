d211954b6a224cdd844c487a84837d2a3da38822
Tim Doerzbacher <tim@tim-doerzbacher.com>
2017-12-17 13:51:40 -0500

    Create sketch skeleton

---
+++ holonomic/holonomic.ino
@@ -0,0 +1,17 @@
+/**
+ * Main loop and callbacks for the holonomic drive
+ */
+
+#include <Stepper.h>
+#include "pins.h"
+#include "motors.h"
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
--- holonomic/main.cpp
+++
@@ -1,7 +0,0 @@
-/**
- * Main loop and callbacks for the holonomic drive
- */
-
-#include <Stepper.h>
-#include "pins.h"
-#include "motors.h"
