cf38160b6b3946d36bd463438795717ec127f4ff
Tim Doerzbacher <tim@tim-doerzbacher.com>
2017-12-17 13:59:17 -0500

    Update pins

--- holonomic/pins.h
+++ holonomic/pins.h
@@ -1,6 +1,8 @@
 #ifndef __CATPOO_PINS_H__
 #define __CATPOO_PINS_H__
 
+#include <Arduino.h>
+
 /****************************************************************************
  * Motor pin out constants.
  *
@@ -11,33 +13,33 @@
 //
 // Left Motor
 //
-#define MOTOR_1_PIN_1 A0
+#define MOTOR_1_PIN_1 12
 #define MOTOR_1_PIN_2 A1
-#define MOTOR_1_PIN_3 A2
-#define MOTOR_1_PIN_4 A3
+#define MOTOR_1_PIN_3 A0
+#define MOTOR_1_PIN_4 13
 
 //
 // Front Motor
 //
-#define MOTOR_2_PIN_1 A4
-#define MOTOR_2_PIN_2 A5
-#define MOTOR_2_PIN_3 4
-#define MOTOR_2_PIN_4 5
+#define MOTOR_2_PIN_1 A5
+#define MOTOR_2_PIN_2 A3
+#define MOTOR_2_PIN_3 A4
+#define MOTOR_2_PIN_4 A2
 
 //
 // Right Motor
 //
-#define MOTOR_3_PIN_1 6
-#define MOTOR_3_PIN_2 7
-#define MOTOR_3_PIN_3 8
-#define MOTOR_3_PIN_4 9
+#define MOTOR_3_PIN_1 3
+#define MOTOR_3_PIN_2 5
+#define MOTOR_3_PIN_3 4
+#define MOTOR_3_PIN_4 6
 
 //
 // Back Motor
 //
-#define MOTOR_4_PIN_1 10
-#define MOTOR_4_PIN_2 11
-#define MOTOR_4_PIN_3 12
-#define MOTOR_4_PIN_4 13
+#define MOTOR_4_PIN_1 8
+#define MOTOR_4_PIN_2 10
+#define MOTOR_4_PIN_3 9
+#define MOTOR_4_PIN_4 11
 
 #endif
