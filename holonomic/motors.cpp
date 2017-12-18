85270a8fc7db622a621eb59283e9f65a8ad1a341
Tim Doerzbacher <tim@tim-doerzbacher.com>
2017-12-17 19:16:26 -0500

    Expand motor helpers

--- holonomic/motors.cpp
+++ holonomic/motors.cpp
@@ -1,7 +1,19 @@
+#include <AccelStepper.h>
+
 #include "motors.h"
 #include "pins.h"
 
-Stepper leftStepper(STEPS_PER_REVOLUTION,  MOTOR_1_PIN_1, MOTOR_1_PIN_2, MOTOR_1_PIN_3, MOTOR_1_PIN_4);
-Stepper frontStepper(STEPS_PER_REVOLUTION, MOTOR_2_PIN_1, MOTOR_2_PIN_2, MOTOR_2_PIN_3, MOTOR_2_PIN_4);
-Stepper rightStepper(STEPS_PER_REVOLUTION, MOTOR_3_PIN_1, MOTOR_3_PIN_2, MOTOR_3_PIN_3, MOTOR_3_PIN_4);
-Stepper backStepper(STEPS_PER_REVOLUTION,  MOTOR_4_PIN_1, MOTOR_4_PIN_2, MOTOR_4_PIN_3, MOTOR_4_PIN_4);
+MultiStepper steppers = MultiStepper();
+AccelStepper leftStepper = AccelStepper(AccelStepper::HALF4WIRE,  MOTOR_1_PIN_1, MOTOR_1_PIN_2, MOTOR_1_PIN_3, MOTOR_1_PIN_4);
+AccelStepper frontStepper = AccelStepper(AccelStepper::HALF4WIRE, MOTOR_2_PIN_1, MOTOR_2_PIN_2, MOTOR_2_PIN_3, MOTOR_2_PIN_4);
+AccelStepper rightStepper = AccelStepper(AccelStepper::HALF4WIRE, MOTOR_3_PIN_1, MOTOR_3_PIN_2, MOTOR_3_PIN_3, MOTOR_3_PIN_4);
+AccelStepper backStepper = AccelStepper(AccelStepper::HALF4WIRE,  MOTOR_4_PIN_1, MOTOR_4_PIN_2, MOTOR_4_PIN_3, MOTOR_4_PIN_4);
+
+void resetAllSteppers()
+{
+  leftStepper.setCurrentPosition(0);
+  frontStepper.setCurrentPosition(0);
+  rightStepper.setCurrentPosition(0);
+  backStepper.setCurrentPosition(0);
+}
+
--- holonomic/motors.h
+++ holonomic/motors.h
@@ -1,13 +1,20 @@
 #ifndef __CATPOO_MOTORS_H__
 #define __CATPOO_MOTORS_H__
 
-#include <Stepper.h>
+#include <AccelStepper.h>
+#include <MultiStepper.h>
 
-#define STEPS_PER_REVOLUTION 4095
+#define STEPS_PER_REVOLUTION 2048
+// #define MAX_SPEED 0.5
+#define MAX_SPEED 1000
+#define MAX_ACCELERATION 100
 
-extern Stepper leftStepper;
-extern Stepper frontStepper;
-extern Stepper rightStepper;
-extern Stepper backStepper;
+extern MultiStepper steppers;
+extern AccelStepper leftStepper;
+extern AccelStepper frontStepper;
+extern AccelStepper rightStepper;
+extern AccelStepper backStepper;
+
+void resetAllSteppers();
 
 #endif
