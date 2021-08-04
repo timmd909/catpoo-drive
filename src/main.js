b48376975d7b5365d4e9e676951fdd351980c64e
Tim Doerzbacher <tim@tim-doerzbacher.com>
2021-08-03 20:15:04 -0400

    enable wheel 1 pin test

--- src/main.js
+++ src/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
 import _ from 'lodash';
+
+const pins = require('gpio-pins');
+
+var gpio = require('rpi-gpio').promise;
+
+gpio.setup(pins.WHEEL_1_ENABLE, gpio.DIR_OUT)
+	.then(() => {
+		return gpio.write(pins.WHEEL_1_ENABLE, true)
+	});
