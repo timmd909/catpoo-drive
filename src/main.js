212fb7030457e7c57d883a2f90f3848218efe259
Tim Doerzbacher <tim@tim-doerzbacher.com>
2021-08-03 21:12:38 -0400

    Update wheel test

--- src/main.js
+++ src/main.js
@@ -1,12 +1,41 @@
+//
+// MASSIVELY UGLY CODE WARNING!
+//
+// JUST VERIFYING THAT WHEELS TURN ON AND THE POWER REQUIREMENTS SPIKE!
+//
+
 'use strict';
 
-import _ from 'lodash';
+const pins = require('./src/gpio-pins');
+console.log('PINS =', pins)
+const gpio = require('rpi-gpio');
 
-const pins = require('gpio-pins');
+gpio.setMode(gpio.MODE_BCM);
 
-var gpio = require('rpi-gpio').promise;
+console.log('Turning on wheels ', pins.WHEEL_1_ENABLE);
 
-gpio.setup(pins.WHEEL_1_ENABLE, gpio.DIR_OUT)
-	.then(() => {
-		return gpio.write(pins.WHEEL_1_ENABLE, true)
-	});
+Promise.resolve(gpio.promise.setup(pins.WHEEL_1_ENABLE, gpio.DIR_OUT))
+Promise.resolve(gpio.promise.setup(pins.WHEEL_2_ENABLE, gpio.DIR_OUT))
+Promise.resolve(gpio.promise.setup(pins.WHEEL_3_ENABLE, gpio.DIR_OUT))
+Promise.resolve(gpio.promise.setup(pins.WHEEL_4_ENABLE, gpio.DIR_OUT))
+
+setTimeout(function () {
+  console.log('ON');
+  gpio.write(pins.WHEEL_1_ENABLE, true);
+  gpio.write(pins.WHEEL_2_ENABLE, true);
+  gpio.write(pins.WHEEL_3_ENABLE, true);
+  gpio.write(pins.WHEEL_4_ENABLE, true);
+}, 1000);
+
+setTimeout(function () {
+  console.log('OFF');
+  gpio.write(pins.WHEEL_1_ENABLE, false);
+  gpio.write(pins.WHEEL_2_ENABLE, false);
+  gpio.write(pins.WHEEL_3_ENABLE, false);
+  gpio.write(pins.WHEEL_4_ENABLE, false);
+
+	setTimeout(function () {
+    console.log('Done');
+    process.exit(0);
+  }, 1000);
+}, 5000);
